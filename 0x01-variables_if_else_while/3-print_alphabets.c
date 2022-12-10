132f9696116f439a7ae9e78091928b45794c72df
tylerwilliam <wiltylerarthurgyamerah@gmail.com>
2022-12-09 18:17:20 -0800

    13

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,18 +9,15 @@
 
 int main(void)
 {
-  char c;
-
-  for (c = 'a' ; c <= 'z' ; c++)
-    {
-      putchar(c);
-
-    }
-
-  for (c = 'A' ; c <= 'Z' ; c++)
-    {
-      putchar(c);
-    }
-  putchar('\n');
-  return (0);
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(c);
+}
+for (c = 'A' ; c <= 'Z' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,16 +9,14 @@
 
 int main(void)
 {
-  char c;
-
-  for (c = 'a' ; c <= 'z' ; c++)
-    {
-      if ((c == 'q' || c == 'e') != 1)
-	{
-	  putchar(c);
-	}
-
-    }
-  putchar('\n');
-  return (0);
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+if ((c == 'q' || c == 'e') != 1)	
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
 }
